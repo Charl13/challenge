c5d8b7fefa584b1837439bf981c982710dc5eafd
Charlie <charlie.vieillard@thesecretlab.nl>
2018-08-14 10:18:27 +0200

    Update user model to match data model.

--- app/User.php
+++ app/User.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Carbon\Carbon;
+use Throwable;
 
 class User extends Authenticatable
 {
@@ -15,15 +17,49 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'address',
+        'date_of_birth',
+        'created_at',
+        'timezone',
+        'interest',
+        'account',
+        'description',
+    ];
+
+    protected $dates = [
+        'date_of_birth',
+        'created_at',
+        'updated_at',
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * Attempt to interpret date format.
      *
-     * @var array
+     * @param mixed $value
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function setDateOfBirthAttribute($value)
+    {
+        try {
+            $date = new Carbon($value);
+            $this->attributes['date_of_birth'] = $date;
+            $this->attributes['timezone'] = $date->format('e');
+        } catch (Throwable $e) {
+            // Unable to handle this format.
+        }
+    }
+
+    /**
+     * Add timezone if available.
+     *
+     * @param mixed $value
+     *
+     * @return Carbon
+     */
+    public function getDateOfBirthAttribute($value)
+    {
+        $date = new Carbon($value);
+        return $this->timezone ? $date->tz($this->timezone) : $date;
+    }
 }
